dacba778286d4ebe8c19d7e35d5a730e3a1601bc
Valentin Grégoire <valentin.gregoire@gmail.com>
2020-12-12 00:41:11 +0100

    status les pot and Hall sensor

--- src/main.py
+++ src/main.py
@@ -3,17 +3,21 @@ import _thread
 import bluetooth
 from machine import ADC
 from machine import Pin
+import machine
 from micropython import const
 from network import WLAN
 from utime import sleep_ms
+import esp32
 
 from ble_advertising import advertising_payload
 
 WLAN(0).active(False)
 WLAN(1).active(False)
 
+machine.freq(240000000)
 
-def create_pin(pin_id: int) -> Pin:
+
+def _create_pin_in(pin_id: int) -> Pin:
     return Pin(pin_id, Pin.IN, Pin.PULL_UP)
 
 
@@ -24,17 +28,25 @@ _CURRENT_PATCH = 1
 
 
 _BUTTON_PIN_MAP = {
-    "button_rig_up": create_pin(32),
-    "button_rig_down": create_pin(33),
-    "button_scene1": create_pin(25),
-    "button_scene2": create_pin(26),
-    "button_scene3": create_pin(27),
-    "button_scene4": create_pin(14),
-    "button_tap_tempo": create_pin(12)
+    "button_rig_up": _create_pin_in(32),
+    "button_rig_down": _create_pin_in(33),
+    "button_scene1": _create_pin_in(25),
+    "button_scene2": _create_pin_in(26),
+    "button_scene3": _create_pin_in(27),
+    "button_scene4": _create_pin_in(14),
+    # "button_tap_tempo": create_pin(12)
 }
 
+_BUTTON_POT = _create_pin_in(12)
 _POT = ADC(Pin(35))
 _POT.atten(ADC.ATTN_11DB)
+_LED_POT_STATUS = Pin(19, Pin.OUT, Pin.PULL_UP)
+_LED_POT_STATUS.value(1)
+
+_BUTTON_HALL_SENSOR = _create_pin_in(22)
+# _BUTTON_HALL_SENSOR.value(1)
+_LED_HALL_SENSOR = Pin(23, Pin.OUT, Pin.PULL_UP)
+_LED_HALL_SENSOR.value(1)
 
 _STATUS_LED = Pin(13, Pin.OUT, Pin.PULL_UP)
 _STATUS_LED.value(0)
@@ -127,25 +139,58 @@ def main():
     remote = BLEHeadrushRemote(ble)
 
     previous_pot_value = -1
-    button_pressed = None
+    button_down = None
+    button_hall_sensor = False
+    button_hall_sensor_down = False
+    button_pot = False
+    button_pot_down = False
     while True:
         if remote.is_connected():
-            pot_value = int(_POT.read() / 32)
-            if previous_pot_value not in [pot_value - 1, pot_value, pot_value + 1]:
-                number = 61
-                previous_pot_value = pot_value
-                remote.send("POT|{}|{}".format(number, pot_value))
+            # foot switches
+            for command, button in _BUTTON_PIN_MAP.items():
+                if not button_down == command and button.value() == 0:
+                    button_down = command
+                    print("sending '{command}'.".format(command=command))
+                    # remote.send(struct.pack("I", command))
+                    remote.send(command)
+                    blink_led(2)
+                    break
+                elif button_down == command and button.value() == 1:
+                    button_down = None
+
+            # POT
+            if _BUTTON_POT.value() == 0 and not button_pot_down:
+                button_pot = not button_pot
+                button_pot_down = True
+            elif _BUTTON_POT.value() == 1 and button_pot_down:
+                button_pot_down = False
+            if button_pot:
+                pot_value = int(_POT.read() / 32)
+                if previous_pot_value not in [pot_value - 1, pot_value, pot_value + 1]:
+                    # wheels: 61, 62, 63
+                    number = 61
+                    previous_pot_value = pot_value
+                    remote.send("POT|{}|{}".format(number, pot_value))
+                if _LED_POT_STATUS.value() == 0:
+                    _LED_POT_STATUS.value(1)
             else:
-                for command, button in _BUTTON_PIN_MAP.items():
-                    if not button_pressed == command and button.value() == 0:
-                        button_pressed = command
-                        print("sending '{command}'.".format(command=command))
-                        # remote.send(struct.pack("I", command))
-                        remote.send(command)
-                        blink_led(2)
-                        break
-                    elif button_pressed == command and button.value() == 1:
-                        button_pressed = None
+                if _LED_POT_STATUS.value() == 1:
+                    _LED_POT_STATUS.value(0)
+
+            # Hall sensor
+            if _BUTTON_HALL_SENSOR.value() == 0 and not button_hall_sensor_down:
+                button_hall_sensor = not button_hall_sensor
+                button_hall_sensor_down = True
+            elif _BUTTON_HALL_SENSOR.value() == 1 and button_hall_sensor_down:
+                button_hall_sensor_down = False
+            if button_hall_sensor:
+                hall_vallue = esp32.hall_sensor()
+                print(hall_vallue)
+                if _LED_HALL_SENSOR.value() == 0:
+                    _LED_HALL_SENSOR.value(1)
+            else:
+                if _LED_HALL_SENSOR.value() == 1:
+                    _LED_HALL_SENSOR.value(0)
 
 
 if __name__ == "__main__":
